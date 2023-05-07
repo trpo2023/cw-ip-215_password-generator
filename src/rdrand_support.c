b1525d123a324ee16acfa13e9440e0b9454a3620
harakki <124341753+harakki@users.noreply.github.com>
2023-05-07 17:20:57 +0700

    Create number generation by CPU instruction RdRand

--- src/rdrand_support.c
+++ src/rdrand_support.c
@@ -1,10 +1,12 @@
-#include <cpuid.h>
-#include <stdio.h>
+#include <cpuid.h> // нужно для процессорных инструкций
+#include <stdio.h> // для printf()
 
-int rdrand_support() {
+int main() {
     unsigned int eax, ebx, ecx, edx;
-    eax = 1;
-    ecx = 0;
-    __cpuid(1, eax, ebx, ecx, edx);
-    int rdrand_support_value = (ecx & 0x40000000) ? 1 : 0; // если инструкция поддерживается, то присваивает rdrand_support_value значение 1, иначе 0
+    eax = 1; // запрос информации о процессоре (https://en.wikipedia.org/wiki/CPUID#EAX=1:_Processor_Info_and_Feature_Bits)
+    ecx = 0; // значение поддержки RdRand
+    __cpuid(1, eax, ebx, ecx, edx); // возвращает информацию о возможностях процессора в eax, ebx, ecx, edx
+    int rdrand_support_value = (ecx & 0x40000000) ? 1 : 0; // если инструкция поддерживается (проверка путём сравнения бита 30 регистра), то присваивает rdrand_support_value значение 1, иначе 0
+    printf("%i\n", rdrand_support_value);
+    return 0;
 }
